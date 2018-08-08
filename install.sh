3782ef64d2e1af22730cb555ec450fdc0abecbb2
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-08-08 14:19:50 +0100

    added install shell script

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+# pip
+sudo apt install python-pip
+# Display libraries
+sudo apt-get install python-dev
+sudo python setup.py install # after downloading gpio setup, see if this is optional
+sudo apt-get install python-smbus
+sudo apt-get install python-serial
+sudo apt-get install python-imaging
+sudo python setup.py install # after downloading spidev, see if this is optional
+# python libraries
+pip install Image
+pip install ImageDraw
+pip install ImageFont
+pip install pygame
