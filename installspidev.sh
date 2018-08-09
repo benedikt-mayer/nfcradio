f800d8f5fdaa238c3d9733dc7f9029d94fffc57b
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-08-09 11:06:10 +0100

    improved spidev script to navigate to home and back

--- installspidev.sh
+++ installspidev.sh
@@ -1,6 +1,8 @@
+$currentdir = echo pwd
+cd ~
 mkdir python-spi
 cd python-spi
 wget https://raw.github.com/doceme/py-spidev/master/setup.py
 wget https://raw.github.com/doceme/py-spidev/master/spidev_module.c
 sudo python setup.py install
-cd ..
+cd $currentdir
