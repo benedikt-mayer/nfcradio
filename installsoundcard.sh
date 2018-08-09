6f63ff982ccb82839151bfc410b829957e8df856
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-08-09 11:52:04 +0100

    improved spacing

--- installsoundcard.sh
+++ installsoundcard.sh
@@ -1,2 +1,2 @@
 sudo touch /etc/asound.conf
-echo "pcm.!default { \n    type hw \n    card 1 \n} \n \n ctl.!default { \n    type hw \n    card 1 \n } \n" >> /etc/asound.conf
+sudo echo "pcm.!default { \n    type hw \n    card 1 \n} \n \nctl.!default { \n    type hw \n    card 1 \n} \n" >> /etc/asound.conf
