da2789fec447e9b83fc70c3858aea2d6dd9e5a96
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-08-09 11:16:28 +0100

    added readme and changelog of spidev

--- installspidev.sh
+++ installspidev.sh
@@ -4,5 +4,7 @@ mkdir python-spi
 cd python-spi
 wget https://raw.github.com/doceme/py-spidev/master/setup.py
 wget https://raw.github.com/doceme/py-spidev/master/spidev_module.c
+wget https://raw.github.com/doceme/py-spidev/master/README.md
+wget https://raw.github.com/doceme/py-spidev/master/CHANGELOG.md
 sudo python setup.py install
 cd $currentdir
