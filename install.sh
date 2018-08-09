abf6e9f2555d58b6b84c837ee6d1cd575e224f5a
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-08-09 13:16:08 +0100

    more swag

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 # edit config file
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "writing i2c information to /etc/modules"
 if ! grep -q "i2c-bcm2708" /etc/modules
 then
     echo "i2c-bcm2708" >> /etc/modules
@@ -6,6 +8,8 @@ else
     echo "i2c data already added"
 fi
 # add soundcard
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "making the usb soundcard the default"
 if ! grep -Pzoq "pcm.!default { \n    type hw \n    card 1 \n} \n \nctl.!default { \n    type hw \n    card 1 \n} \n" /etc/asound.conf
 then
     touch /etc/asound.conf
@@ -15,15 +19,23 @@ else
     echo "soundcard configuration data already added"
 fi
 # update apt-get just in case
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "updating apt-get"
 sudo apt-get update
 # pip
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "installing pip"
 sudo apt install python-pip
 # display libraries
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "installing python python dependencies"
 sudo apt-get install python-dev
 sudo apt-get install python-smbus
 sudo apt-get install python-serial
 sudo apt-get install python-imaging
 # install spi-dev
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "installing spi-dev"
 $currentdir = echo pwd
 cd ~
 mkdir python-spi
@@ -35,9 +47,13 @@ wget https://raw.github.com/doceme/py-spidev/master/CHANGELOG.md
 sudo python setup.py install
 cd $currentdir
 # python libraries
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "installing python libraries"
 pip install Pillow
 pip install pygame
 # add our scripts to autostart
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "adding scripts to autostart"
 run_autostart_script()
 {
     ANSWER=$1
@@ -65,6 +81,8 @@ run_autostart_script()
         run_autostart_script $user_input
     fi
 }
-echo "setting up autostart. Are you running on the master or slave?"
+echo "setting up autostart. are you running on the master or slave?"
 read user_input
 run_autostart_script $user_input
+tput setaf 2;echo "-------------------------------"
+tput sgr0;echo "done"
