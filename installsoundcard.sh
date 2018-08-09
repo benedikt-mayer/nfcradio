e7b76fc42d238b3c6d01510209ffd017694b03c2
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-08-09 11:50:57 +0100

    added proper rights to sound card adding script

--- installsoundcard.sh
+++ installsoundcard.sh
@@ -1,1 +1,2 @@
-echo "pcm.!default { \n    type hw \n    card 1 \n} \n \n ctl.!default { \n    type hw \n    card 1 \n } \n" > /etc/asound.conf
+sudo touch /etc/asound.conf
+echo "pcm.!default { \n    type hw \n    card 1 \n} \n \n ctl.!default { \n    type hw \n    card 1 \n } \n" >> /etc/asound.conf
